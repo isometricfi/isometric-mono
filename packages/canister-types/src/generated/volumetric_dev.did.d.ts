761ffe564aaa903cbc6dfd75f40e2779145937a0
Dylan van Heerden <61427891+dylanvanh@users.noreply.github.com>
2025-12-08 19:09:14 +0200

    feat(canister): regen .did and js files

--- packages/canister-types/src/generated/volumetric_dev.did.d.ts
+++ packages/canister-types/src/generated/volumetric_dev.did.d.ts
@@ -2,6 +2,20 @@ import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 import type { IDL } from '@dfinity/candid';
 
-export interface _SERVICE { 'greet' : ActorMethod<[string], string> }
+export interface Config { 'temp' : string }
+export type Result = { 'Ok' : null } |
+  { 'Err' : VolumetricError };
+export type VolumetricError = { 'Internal' : string } |
+  { 'UnauthorizedWhitelisted' : { 'caller' : string } } |
+  { 'UnauthorizedController' : { 'caller' : string } } |
+  { 'ConfigError' : string };
+export interface _SERVICE {
+  'add_whitelisted' : ActorMethod<[Principal], Result>,
+  'get_config' : ActorMethod<[], Config>,
+  'greet' : ActorMethod<[string], string>,
+  'list_whitelisted' : ActorMethod<[], Array<Principal>>,
+  'remove_whitelisted' : ActorMethod<[Principal], Result>,
+  'set_temp' : ActorMethod<[string], Result>,
+}
 export declare const idlFactory: IDL.InterfaceFactory;
 export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
--- packages/canister-types/src/generated/volumetric_dev.did.js
+++ packages/canister-types/src/generated/volumetric_dev.did.js
@@ -1,4 +1,19 @@
 export const idlFactory = ({ IDL }) => {
-  return IDL.Service({ 'greet' : IDL.Func([IDL.Text], [IDL.Text], ['query']) });
+  const VolumetricError = IDL.Variant({
+    'Internal' : IDL.Text,
+    'UnauthorizedWhitelisted' : IDL.Record({ 'caller' : IDL.Text }),
+    'UnauthorizedController' : IDL.Record({ 'caller' : IDL.Text }),
+    'ConfigError' : IDL.Text,
+  });
+  const Result = IDL.Variant({ 'Ok' : IDL.Null, 'Err' : VolumetricError });
+  const Config = IDL.Record({ 'temp' : IDL.Text });
+  return IDL.Service({
+    'add_whitelisted' : IDL.Func([IDL.Principal], [Result], []),
+    'get_config' : IDL.Func([], [Config], ['query']),
+    'greet' : IDL.Func([IDL.Text], [IDL.Text], ['query']),
+    'list_whitelisted' : IDL.Func([], [IDL.Vec(IDL.Principal)], ['query']),
+    'remove_whitelisted' : IDL.Func([IDL.Principal], [Result], []),
+    'set_temp' : IDL.Func([IDL.Text], [Result], []),
+  });
 };
 export const init = ({ IDL }) => { return []; };
