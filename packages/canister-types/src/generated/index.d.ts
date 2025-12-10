d33a95a7c0d85d73ecc9df4398b02beb6ee5ec9f
Dylan van Heerden <61427891+dylanvanh@users.noreply.github.com>
2025-12-11 00:33:37 +0200

    feat(web): add btc auth components and testing ui

--- packages/canister-types/src/generated/index.d.ts
+++ packages/canister-types/src/generated/index.d.ts
@@ -7,7 +7,7 @@ import type {
 import type { Principal } from "@dfinity/principal";
 import type { IDL } from "@dfinity/candid";
 
-import { _SERVICE } from './volumetric_dev.did';
+export { _SERVICE, UserInfo } from './volumetric_dev.did';
 
 export declare const idlFactory: IDL.InterfaceFactory;
 export declare const canisterId: string;
