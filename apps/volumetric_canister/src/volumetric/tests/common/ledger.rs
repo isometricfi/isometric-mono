8b6e9a2b625654249f3e872e4e61b14795613236
Dylan van Heerden <61427891+dylanvanh@users.noreply.github.com>
2026-04-07 13:27:00 +0200

    test(canister): cover ledger transfer dedup

--- apps/volumetric_canister/src/volumetric/tests/common/ledger.rs
+++ apps/volumetric_canister/src/volumetric/tests/common/ledger.rs
@@ -1,6 +1,6 @@
 use candid::Nat;
 use icrc_ledger_types::icrc1::account::Account;
-use icrc_ledger_types::icrc1::transfer::{TransferArg, TransferError};
+use icrc_ledger_types::icrc1::transfer::{Memo, TransferArg, TransferError};
 
 use super::TestEnv;
 
@@ -12,7 +12,7 @@ pub fn balance_of(env: &TestEnv, account: Account) -> u64 {
 
 #[allow(dead_code)]
 pub fn mint(env: &TestEnv, to: Account, amount: u64) -> Result<u64, TransferError> {
-    transfer(env, env.minter_canister, to, amount, None)
+    transfer_with_dedup_fields(env, env.minter_canister, to, amount, None, None, None)
 }
 
 #[allow(dead_code)]
@@ -23,13 +23,26 @@ pub fn transfer(
     amount: u64,
     from_subaccount: Option<[u8; 32]>,
 ) -> Result<u64, TransferError> {
+    transfer_with_dedup_fields(env, from, to, amount, from_subaccount, None, None)
+}
+
+#[allow(dead_code)]
+pub fn transfer_with_dedup_fields(
+    env: &TestEnv,
+    from: candid::Principal,
+    to: Account,
+    amount: u64,
+    from_subaccount: Option<[u8; 32]>,
+    memo_bytes: Option<Vec<u8>>,
+    created_at_time_ns: Option<u64>,
+) -> Result<u64, TransferError> {
     let args = TransferArg {
         from_subaccount,
         to,
         amount: Nat::from(amount),
         fee: None,
-        memo: None,
-        created_at_time: None,
+        memo: memo_bytes.map(Memo::from),
+        created_at_time: created_at_time_ns,
     };
 
     let result: Result<Nat, TransferError> = env
