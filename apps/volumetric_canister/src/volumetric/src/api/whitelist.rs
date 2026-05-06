6d0846113b53034704b24a1b50e65ef7b0dc1ce7
Dylan van Heerden <61427891+dylanvanh@users.noreply.github.com>
2026-05-06 20:19:16 +0200

    refactor: whitelist config protection , hello method (#198)

--- apps/volumetric_canister/src/volumetric/src/api/whitelist.rs
+++ apps/volumetric_canister/src/volumetric/src/api/whitelist.rs
@@ -5,6 +5,14 @@ use crate::errors::VolumetricError;
 use crate::guards::{is_controller, is_whitelisted, no_replicated_call};
 use crate::usecases;
 
+const HELLO_RESPONSE: &str = "hello";
+
+#[query(guard = "no_replicated_call")]
+pub fn hello() -> Result<String, VolumetricError> {
+    is_whitelisted()?;
+    Ok(HELLO_RESPONSE.to_string())
+}
+
 #[update]
 pub fn add_whitelisted(principal: Principal) -> Result<(), VolumetricError> {
     is_controller()?;
